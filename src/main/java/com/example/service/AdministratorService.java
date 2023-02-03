9605b2c9fe947a229887836ea6b824d490b69219
nmnm1016 <s.nomura.wk@gmail.com>
2023-02-03 15:10:21 +0900

    管理者登録画面を表示する機能の作成

---
+++ src/main/java/com/example/service/AdministratorService.java
@@ -0,0 +1,17 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Administrator;
+
+@Service
+@Transactional
+
+public class AdministratorService {
+
+	@Autowired
+	private Administrator administrator;
+
+}
