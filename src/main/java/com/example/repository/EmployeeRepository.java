e1e16b25cb1c28eb6d28dd749fd28dcd7635b123
nmnm1016 <s.nomura.wk@gmail.com>
2023-02-06 14:40:08 +0900

    8章完成　detail.htmlを作成

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -64,8 +64,8 @@ public class EmployeeRepository {
 
 	public Employee load(Integer id) {
 
-		String sql = "SELECT id, name, image, gender, hire_date, mail_address, zip_code, \"\n"
-				+ "				+ \" address, telephone, salary, characteristics, dependents_count FROM employees  WHERE id=:id";
+		String sql = "SELECT id, name, image, gender, hire_date, mail_address, zip_code, "
+				+ "address, telephone, salary, characteristics, dependents_count FROM employees  WHERE id=:id";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 
 		Employee employee = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
